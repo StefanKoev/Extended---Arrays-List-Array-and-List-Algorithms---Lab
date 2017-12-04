bd3718a46b46a284dc0e8b712ecee23b663cfc42
Stefan Koev <koev.stefan@gmail.com>
2017-12-04 18:44:44 +0200

    Create 02.Odd-Numbers-at-Odd-Positions

---
+++ 02.Odd-Numbers-at-Odd-Positins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Odd_Numbers_at_Odd_Positins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string prefix = "Index";
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 != 0 && numbers[i] % 2 != 0)
+                {
+                    Console.WriteLine($"{prefix} {i} -> {numbers[i]}");
+                }
+            }
+        }
+    }
+}
