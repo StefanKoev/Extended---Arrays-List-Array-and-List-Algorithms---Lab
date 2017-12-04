24010d6e43108856c32d04559c53c4328145323f
Stefan Koev <koev.stefan@gmail.com>
2017-12-04 17:56:42 +0200

    Create 01.Rotate-Array-of-String

---
+++ 01.Rotate-Array-of-String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Rotate_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+
+            string temp = array[array.Length - 1];
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[0] = temp;
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
