4a6f7a26a209e51e7497548a6ab5ac176dd2f0d7
Stefan Koev <koev.stefan@gmail.com>
2017-12-04 20:24:19 +0200

    Create 03.Array-Contains-Elements

---
+++ 03.Array-Contains-Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Array_Contains_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            bool isFound = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (n == numbers[i])
+                {
+                    isFound = true;
+                    break;
+                }
+            }
+            if (isFound == true)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
