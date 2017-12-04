6d5d395da7e34e54eedc2c1266d1a125ac9e7093
Stefan Koev <koev.stefan@gmail.com>
2017-12-04 21:19:48 +0200

    Create 05.Largest-N-Elements

---
+++ 05.Largest-N-Elements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Largest_N_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            numbers.Sort();
+
+            List<int> resultNumbers = new List<int>();
+
+            for (int i = numbers.Count; i >= numbers.Count - n; i--)
+            {
+                resultNumbers.Add(numbers[i]);
+            }
+
+            Console.WriteLine(String.Join(" ", resultNumbers));
+        }
+    }
+}
